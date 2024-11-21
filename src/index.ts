64b7d011b7bfdb7129e050ce59acf8284b3f2674
adilraja11 <adilraja19@students.undip.ac.id>
2024-11-21 10:37:40 +0700

    implement authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Elysia, t } from "elysia";
 import { swagger } from '@elysiajs/swagger'
 import { note } from "./routes/note";
+import { user } from "./routes/user";
 
 const app = new Elysia()
   // apply the swagger plugin
   .use(swagger())
   .use(note)
+  .use(user)
   .listen(3000);
 
 console.log(
